ce54033535d0e33b91f5e794683ba7481800b770
Alex Goodwin <agoodwin727@gmail.com>
2017-01-02 14:40:35 -0700

    Added initial resource node

--- game/game.js
+++ game/game.js
@@ -26,7 +26,7 @@ const CAMERA_START_Y = MAPLENGTH / 2;
 const CAMERA_START_Z = 2000;
 
 /* Interface Settings */
-const MAXZOOM = 3000;
+const MAXZOOM = 2500;
 const MINZOOM = 100;
 
 class Game{
@@ -43,11 +43,15 @@ class Game{
       this.phi = 60;
 
       this.cubes = [];
+      this.resourceNodes = [];
 
-      this.addCubes();
       this.addGround();
 
+      this.addMenu();
+
       this.watchEvents();
+
+      this.scenario1();
     }
 
     update() {
@@ -64,6 +68,7 @@ class Game{
 
     render() {
         this.update();
+        // requestAnimationFrame(this.render());
         this.renderer.render(this.scene, this.camera);
     }
 
@@ -96,7 +101,7 @@ class Game{
     @coordinates: (x, y, z) vector
     @size: (x, y, z) vector
     */
-    addCube(coordinates = new THREE.Vector3(0, 0, 0), size = new THREE.Vector3(100, 100, 100), name = `cube${Math.floor(Math.random()) * 100}`) {
+    addCube(coordinates = new THREE.Vector3(0, 0, 0), size = new THREE.Vector3(100, 100, 100), name = `cube${this.cubes.length}`) {
       let cube = new Cube(size);
 
       cube.name = name;
@@ -108,18 +113,38 @@ class Game{
       cube.setName(name);
       cube.setScene(this.scene);
       cube.setObject(this.scene.getObjectByName(name));
+
       // console.log(`added cube to scene at position (${cube.position.x}, ${cube.position.y}, ${cube.position.z}) using coordinates (${coordinates.x}, ${coordinates.y}, ${coordinates.z})`);
     }
 
     /*
-      Adds 100 randomly sized cubes in random places
+    @coordinates: (x, y, z) vector
+    @size: (x, y, z) vector
     */
-    addCubes(coordinates = new THREE.Vector3(0, 0, 0), number = 1000) {
+    addResourceNode(coordinates = new THREE.Vector3(0, 0, 0), size = new THREE.Vector3(100, 100, 100), name = `resourceNode${this.resourceNodes.length}`) {
+      let resourceNode = new ResourceNode();
+
+      resourceNode.name = name;
+      resourceNode.position.set(coordinates.x, coordinates.y, coordinates.z);
+
+      this.scene.add(resourceNode);
+      this.resourceNodes.push(resourceNode);
+
+      resourceNode.setName(name);
+      resourceNode.setScene(this.scene);
+      resourceNode.setObject(this.scene.getObjectByName(name));
+    }
+
+
+    /*
+      Adds 1000 randomly sized cubes in random places
+    */
+    addRandomCubes(coordinates = new THREE.Vector3(0, 0, 0), number = 1000) {
       for(let i = 0; i < number; i++) {
         let random = Math.random();
         let width = random * 100;
         let length = random * 100;
-        let height = 100;
+        let height = 10;
         let size = new THREE.Vector3(width, length, height);
 
         coordinates = new THREE.Vector3(Math.random() * MAPWIDTH, Math.random() * MAPLENGTH, random * 50);
@@ -130,11 +155,54 @@ class Game{
       }
     }
 
+    scenario1() {
+      // this.addRandomCubes(null, 100);
+
+      // add 4 home cubes
+      let size = new THREE.Vector3(50, 50, 50);
+      let coordinates = new THREE.Vector3(500, 300, 25);
+      this.addCube(coordinates, size, 'soldier1');
+
+      coordinates = new THREE.Vector3(400, 300, 25);
+      this.addCube(coordinates, size, 'soldier2');
+
+      coordinates = new THREE.Vector3(400, 300, 25);
+      this.addCube(coordinates, size, 'soldier3');
+
+      coordinates = new THREE.Vector3(400, 300, 25);
+      this.addCube(coordinates, size, 'soldier4');
+
+      // add resource node
+      coordinates = new THREE.Vector3(1000, 300, 25);
+      this.addResourceNode(coordinates, size);
+    }
+
     addGround() {
       let ground = new Ground();
       ground.name = "ground";
       ground.position.set(MAPWIDTH/2, MAPLENGTH/2, 0);
       this.scene.add(ground);
+    }
+
+    addMenu() {
+      this.menu = new Menu();
+    }
+
+    addUnit() {
+      let size = new THREE.Vector3(50, 50, 50);
+      let coordinates = new THREE.Vector3(500, 300, 25);
+      this.addCube(coordinates, size, `soldier${this.cubes.length}`);
+    }
+
+    removeCube(sceneObject) {
+      this.cubes = this.cubes.filter((cube) => {
+        if(cube.name === sceneObject.name) {
+          this.scene.remove(sceneObject);
+          return false;
+        } else {
+          return true;
+        }
+      });
     }
 
     drawRaycaster() {
@@ -193,15 +261,15 @@ class Game{
 
     initializeLight() {
       let light = new THREE.DirectionalLight(0xffffff, 1);
-      light.position.set(0, MAPLENGTH, 500);
+      light.position.set(0, 0, 2500);
       light.castShadow = true;
       this.scene.add(light);
     }
 
     initializeMouse() {
-      this.mouse = {x: 0, y: 0};
+      this.mouse = new THREE.Vector2();
       this.isMouseDown = false;
-      this.mouseDownPosition = {x: 0, y: 0};
+      this.mouseDownPosition = new THREE.Vector2();
       this.raycaster = new THREE.Raycaster();
     }
 
@@ -287,31 +355,31 @@ class Game{
       if(!this.shiftIsDown) {
         // get intersecting point with ground & add a cube there
 
-      	// update the picking ray with the camera and mouse position
-      	this.raycaster.setFromCamera(this.mouse, this.camera);
+        // update the picking ray with the camera and mouse position
+        this.raycaster.setFromCamera(this.mouse, this.camera);
 
-      	// calculate objects intersecting the picking ray
-      	let intersects = this.raycaster.intersectObjects(this.scene.children);
+        // calculate objects intersecting the picking ray
+        let intersects = this.raycaster.intersectObjects(this.scene.children);
 
         // loop through intersecting objects
-      	for(let intersect of intersects) {
+        for(let intersect of intersects) {
           if(intersect.object.type == "Cube") {
-            this.scene.remove(intersect.object);
+            this.removeCube(intersect.object);
             break;
           }
           // add cube on ground where user clicks
           else if(intersect.object.name == "ground") {
             this.addCube(new THREE.Vector3(intersect.point.x, intersect.point.y, 0), new THREE.Vector3(50, 50, 10), `cube${this.cubes.length}`);
           }
-      	}
+        }
       }
     }
 
     onDocumentMouseMove(event) {
       event.preventDefault();
 
-      this.mouse.x = (event.clientX / this.renderer.domElement.width) * 2 - 1;
-      this.mouse.y = -(event.clientY / this.renderer.domElement.height) * 2 + 1;
+      this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
       if (this.isMouseDown) {
         let oldX = this.mouseDownPosition.x,
@@ -364,7 +432,37 @@ class Game{
 
 module.exports = Game;
 
-class Ground extends THREE.Mesh {
+class SceneObject extends THREE.Mesh {
+  constructor(geometry, material) {
+    super(geometry, material);
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setObject(sceneObject) {
+    this.sceneObject = sceneObject;
+  }
+
+  getObject() {
+    return this.sceneObject;
+  }
+
+  setScene(scene) {
+    this.scene = scene;
+  }
+
+  getScene() {
+    return this.scene;
+  }
+}
+
+class Ground extends SceneObject {
   constructor() {
     let geometry = new THREE.PlaneBufferGeometry(MAPWIDTH, MAPLENGTH, 1, 1);
     let material = new THREE.MeshPhongMaterial({
@@ -377,7 +475,7 @@ class Ground extends THREE.Mesh {
   }
 }
 
-class Cube extends THREE.Mesh {
+class Cube extends SceneObject {
   constructor(size) {
     let geometry = new THREE.BoxGeometry(size.x, size.y, size.z);
     let material = new THREE.MeshLambertMaterial({color: 0xCC0000});
@@ -388,7 +486,8 @@ class Cube extends THREE.Mesh {
     this.castShadow = true;
     this.receiveShadow = true;
 
-    this.velocity = new THREE.Vector3(5, 5, 0);
+    this.velocity = new THREE.Vector3(0, 0, 0);
+    this.speed = 15;
   }
 
   update() {
@@ -425,34 +524,75 @@ class Cube extends THREE.Mesh {
         }
       }
 
+      // move towards last click point
+      let worldX = window.game.raycaster.setFromCamera(window.game.mouse, window.game.camera);
+
+      // calculate objects intersecting the picking ray
+      let intersects = window.game.raycaster.intersectObjects(this.scene.children);
+
+      let mouseWorldCoordinatesX = 0;
+      let mouseWorldCoordinatesY = 0;
+      let mouseWorldCoordinatesZ = 0;
+
+      for(let i in intersects) {
+        if(intersects[i].object.name == "ground") {
+          mouseWorldCoordinatesX = intersects[i].point.x;
+          mouseWorldCoordinatesY = intersects[i].point.y;
+          mouseWorldCoordinatesZ = intersects[i].point.z;
+          break;
+        }
+      }
+
+      let mouseDifX = this.sceneObject.position.x - mouseWorldCoordinatesX;
+      let mouseDifY = this.sceneObject.position.y - mouseWorldCoordinatesY;
+      let mouseDifZ = this.sceneObject.position.y - mouseWorldCoordinatesZ;
+
+      if(Math.abs(mouseDifX) > 10) {
+        this.velocity.x = this.speed * -Math.sign(mouseDifX);
+      } else {
+        this.velocity.x = 0;
+      }
+      if(Math.abs(mouseDifY) > 10) {
+        this.velocity.y = this.speed * -Math.sign(mouseDifY);
+      } else {
+        this.velocity.y = 0;
+      }
+      if(Math.abs(mouseDifZ) > 10) {
+        this.velocity.z = this.speed * -Math.sign(mouseDifZ);
+      } else {
+        this.velocity.z = 0;
+      }
+
       // update position
       this.sceneObject.position.x += this.velocity.x;
       this.sceneObject.position.y += this.velocity.y;
     }
   }
+}
 
-  setName(name) {
-    this.name = name;
+class ResourceNode extends SceneObject {
+  constructor(type = "metal") {
+    let size = 50;
+    let widthSegments = 5;
+    let heightSegments = 5;
+    let geometry = new THREE.SphereGeometry(size, widthSegments, heightSegments);
+    let material = new THREE.MeshLambertMaterial({
+      color: 0xcc44ac
+    });
+
+    super(geometry, material);
+
+    this.type = type;
+    this.castShadow = true;
+    this.receiveShadow = true;
+
+    this.velocity = new THREE.Vector3(0, 0, 0);
+    this.speed = 0;
   }
+}
 
-  getName() {
-    return this.name;
+class Menu {
+  constructor() {
+    this.element = document.getElementById('menu');
   }
-
-  setObject(sceneObject) {
-    this.sceneObject = sceneObject;
-  }
-
-  getObject() {
-    return this.sceneObject;
-  }
-
-  setScene(scene) {
-    this.scene = scene;
-  }
-
-  getScene() {
-    return this.scene;
-  }
-
 }
