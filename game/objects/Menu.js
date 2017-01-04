b7722cebdd67f4f8906133e379fd10090826b98e
Alex Goodwin <agoodwin727@gmail.com>
2017-01-04 00:25:30 -0700

    Abstracted menu buttons

--- game/objects/Menu.js
+++ game/objects/Menu.js
@@ -9,48 +9,52 @@ class Menu {
   constructor(game) {
     this.element = window.document.getElementById('menu');
     this.game = game;
+
+    this.buttons = [
+      {
+        name: 'addUnit',
+        function: this.game.addUnit
+      }, {
+        name: 'addRandomCubes',
+        function: this.game.addRandomCubes
+      }, {
+        name: 'removeAllCubes',
+        function: this.game.removeAllCubes
+      }, {
+        name: 'removeSelectedCubes',
+        function: this.game.removeSelectedCubes
+      }, {
+        name: 'listAllCubes',
+        function: this.game.listAllCubes
+      }, {
+        name: 'resetScore',
+        function: this.game.resetScore
+      }, {
+        name: 'resetResources',
+        function: this.game.resetResources
+      }
+    ];
+
     this.assignElements();
     this.assignClickListeners();
   }
 
   assignElements() {
-    this.addUnitButton = window.document.getElementById('menu-addUnit');
-    this.addRandomCubesButton = window.document.getElementById('menu-addRandomCubes');
-    this.removeAllCubesButton = window.document.getElementById('menu-removeAllCubes');
-    this.removeSelectedCubesButton = window.document.getElementById('menu-removeSelectedCubes');
-    this.listAllCubesButton = window.document.getElementById('menu-listAllCubes');
-    this.resetScoreButton = window.document.getElementById('menu-resetScore');
-    this.resetResourcesButton = window.document.getElementById('menu-resetResources');
+    for(let i in this.buttons) {
+      this.buttons[i].element = window.document.getElementById(`menu-${this.buttons[i].name}`);
+    }
   }
 
   assignClickListeners() {
-    this.addUnitButton.addEventListener('click', () => {
-      this.game.addUnit();
-    }, false );
+    this.element.addEventListener('click', (event) => {
 
-    this.addRandomCubesButton.addEventListener('click', () => {
-      this.game.addRandomCubes();
-    }, false );
-
-    this.removeAllCubesButton.addEventListener('click', () => {
-      this.game.removeAllCubes();
-    }, false );
-
-    this.removeSelectedCubesButton.addEventListener('click', () => {
-      this.game.removeSelectedCubes();
-    }, false );
-
-    this.listAllCubesButton.addEventListener('click', () => {
-      this.game.listAllCubes();
-    }, false );
-
-    this.resetScoreButton.addEventListener('click', () => {
-      this.game.resetScore();
-    }, false );
-
-    this.resetResourcesButton.addEventListener('click', () => {
-      this.game.resetResources();
-    }, false );
+      // if the id matches one in this.buttons, call that button's function
+      for(let i in this.buttons) {
+        if(event.path[0].id === `menu-${this.buttons[i].name}`) {
+          this.buttons[i].function.call(this.game);
+        }
+      }
+    });
   }
 
   updateFood(food) {
