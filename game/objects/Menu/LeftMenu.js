b1ccdf1a243dba0aaa2552d5a8c43cd07109646b
Alex Goodwin <agoodwin727@gmail.com>
2017-01-05 02:13:55 -0700

    Cleaned up menu code and appearance

--- game/objects/Menu/LeftMenu.js
+++ game/objects/Menu/LeftMenu.js
@@ -12,120 +12,118 @@ class LeftMenu extends Menu {
   constructor(game) {
     super(game, 'leftMenu');
 
-    this.buttons = [
-      {
-        name: 'addCube',
-        function: this.game.addUnit
-      }, {
-        name: 'addRandomCubes',
-        function: this.game.addRandomCubes,
-        args: [10]
-      }, {
-        name: 'removeAllCubes',
-        function: this.game.removeAllCubes
-      }, {
-        name: 'removeSelectedCubes',
-        function: this.game.removeSelectedCubes
-      }, {
-        name: 'listAllCubes',
-        function: this.game.listAllCubes
-      }, {
-        name: 'listAllBuildings',
-        function: this.game.listAllBuildings
-      }, {
-        name: 'resetScore',
-        function: this.game.resetScore
-      }, {
-        name: 'resetResources',
-        function: this.game.resetResources
-      }, {
-        name: 'listSelectedUnits',
-        function: this.game.listSelectedUnits
-      }, {
-        name: 'toolToAddRandomNode',
-        function: this.game.setRightTool,
-        args: ['createRandomNode']
-      }, {
-        name: 'toolToAddGold',
-        function: this.game.setRightTool,
-        args: [
-          'createResourceNode', [
-            undefined,
-            new THREE.Vector3(50, 50, 10),
-            'gold'
-          ]
+    this.buttons = [{
+      name: 'addCube',
+      function: this.game.addUnit
+    }, {
+      name: 'addRandomCubes',
+      function: this.game.addRandomCubes,
+      args: [10]
+    }, {
+      name: 'removeAllCubes',
+      function: this.game.removeAllCubes
+    }, {
+      name: 'removeSelectedCubes',
+      function: this.game.removeSelectedCubes
+    }, {
+      name: 'listAllCubes',
+      function: this.game.listAllCubes
+    }, {
+      name: 'listAllBuildings',
+      function: this.game.listAllBuildings
+    }, {
+      name: 'resetScore',
+      function: this.game.resetScore
+    }, {
+      name: 'resetResources',
+      function: this.game.resetResources
+    }, {
+      name: 'listSelectedUnits',
+      function: this.game.listSelectedUnits
+    }, {
+      name: 'toolToAddRandomNode',
+      function: this.game.setRightTool,
+      args: ['createRandomNode']
+    }, {
+      name: 'toolToAddGold',
+      function: this.game.setRightTool,
+      args: [
+        'createResourceNode', [
+          undefined,
+          new THREE.Vector3(50, 50, 10),
+          'gold'
         ]
-      }, {
-        name: 'toolToAddFood',
-        function: this.game.setRightTool,
-        args: [
-          'createResourceNode', [
-            undefined,
-            new THREE.Vector3(50, 50, 10),
-            'food'
-          ]
+      ]
+    }, {
+      name: 'toolToAddFood',
+      function: this.game.setRightTool,
+      args: [
+        'createResourceNode', [
+          undefined,
+          new THREE.Vector3(50, 50, 10),
+          'food'
         ]
-      }, {
-        name: 'toolToAddMetal',
-        function: this.game.setRightTool,
-        args: [
-          'createResourceNode', [
-            undefined,
-            new THREE.Vector3(50, 50, 10),
-            'metal'
-          ]
+      ]
+    }, {
+      name: 'toolToAddMetal',
+      function: this.game.setRightTool,
+      args: [
+        'createResourceNode', [
+          undefined,
+          new THREE.Vector3(50, 50, 10),
+          'metal'
         ]
-      }, {
-        name: 'toolToAddBuilding',
-        function: this.game.setRightTool,
-        args: ['createBuilding']
-      }, {
-        name: 'toolToAddSmCube',
-        function: this.game.setRightTool,
-        args: [
-          'createCube', [
-            undefined,
-            new THREE.Vector3(100, 100, 100),
-            undefined
-          ]
+      ]
+    }, {
+      name: 'toolToAddBuilding',
+      function: this.game.setRightTool,
+      args: ['createBuilding']
+    }, {
+      name: 'toolToAddSmCube',
+      function: this.game.setRightTool,
+      args: [
+        'createCube', [
+          undefined,
+          new THREE.Vector3(100, 100, 100),
+          undefined
         ]
-      }, {
-        name: 'toolToAddMdCube',
-        function: this.game.setRightTool,
-        args: [
-          // tool type
-          'createCube', [
-            // addCube(args)
-            undefined,
-            new THREE.Vector3(250, 250, 250),
-            undefined
-          ]
+      ]
+    }, {
+      name: 'toolToAddMdCube',
+      function: this.game.setRightTool,
+      args: [
+        // tool type
+        'createCube', [
+          // addCube(args)
+          undefined,
+          new THREE.Vector3(250, 250, 250),
+          undefined
         ]
-      }, {
-        name: 'toolToAddLgCube',
-        function: this.game.setRightTool,
-        args: [
-          // tool type
-          'createCube', [
-            // addCube(args)
-            undefined,
-            new THREE.Vector3(500, 500, 500),
-            undefined
-          ]
+      ]
+    }, {
+      name: 'toolToAddLgCube',
+      function: this.game.setRightTool,
+      args: [
+        // tool type
+        'createCube', [
+          // addCube(args)
+          undefined,
+          new THREE.Vector3(500, 500, 500),
+          undefined
         ]
-      }, {
-        name: 'toolToBuildBuilding',
-        function: this.game.setRightTool,
-        args: ['buildBuilding']
-      }
-    ];
+      ]
+    }, {
+      name: 'toolToBuildBuilding',
+      function: this.game.setRightTool,
+      args: ['buildBuilding']
+    }];
 
     this.assignElements();
     this.assignClickListeners();
   }
 
   assignElements() {
-    for(let i in this.buttons) {
+    for (let i in this.buttons) {
       this.buttons[i].element = window.document.getElementById(`menu-${this.buttons[i].name}`);
     }
   }
@@ -133,8 +131,8 @@ class LeftMenu extends Menu {
   assignClickListeners() {
     this.element.addEventListener('click', (event) => {
       // if the id matches one in this.buttons, call that button's function with its args
-      for(let i in this.buttons) {
-        if(event.path[0].id === `menu-${this.buttons[i].name}`) {
+      for (let i in this.buttons) {
+        if (event.path[0].id === `menu-${this.buttons[i].name}`) {
           this.buttons[i].function.apply(this.game, this.buttons[i].args);
         }
       }
@@ -143,19 +141,23 @@ class LeftMenu extends Menu {
   }
 
   updateFood(food) {
-    window.document.getElementById('player-food').innerHTML = parseInt(food);
+    window.document.getElementById('player-food')
+      .innerHTML = parseInt(food);
   }
 
   updateGold(gold) {
-    window.document.getElementById('player-gold').innerHTML = parseInt(gold);
+    window.document.getElementById('player-gold')
+      .innerHTML = parseInt(gold);
   }
 
   updateMetal(metal) {
-    window.document.getElementById('player-metal').innerHTML = parseInt(metal);
+    window.document.getElementById('player-metal')
+      .innerHTML = parseInt(metal);
   }
 
   updateScore(score) {
-    window.document.getElementById('player-score').innerHTML = parseInt(score);
+    window.document.getElementById('player-score')
+      .innerHTML = parseInt(score);
   }
 }
 
